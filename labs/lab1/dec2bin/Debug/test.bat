16ad6378b81b4e1b6a4b10fb050bd1ea0f7a4572
Michael <moskvichevmihail60@yandex.ru>
2018-02-06 23:00:32 +0300

    Fixed tests

--- labs/lab1/dec2bin/Debug/test.bat
+++ labs/lab1/dec2bin/Debug/test.bat
@@ -1,19 +1,19 @@
-set PROGRAM="%~1"
-
-rem Проверка на запуск программы без указания значения
+REM Проверка на запуск программы без указания значения
 dec2bin.exe 
 if NOT ERRORLEVEL 1 goto err
 
-rem Проверка на запуск программы с неверным значением
+REM Проверка на запуск программы с неверным значением
 dec2bin.exe string
 if NOT ERRORLEVEL 1 goto err
 
-rem Проверка на запуск программы с отрицательным значением
+REM Проверка на запуск программы с отрицательным значением
 dec2bin.exe -253
 if NOT ERRORLEVEL 1 goto err
 
-rem Проверка на запуск программы с верным значением
-dec2bin.exe 15
+REM Проверка на запуск программы с верным значением
+dec2bin.exe 15 > output.txt
+if ERRORLEVEL 1 goto err
+FC /B output.txt 1111.txt
 if ERRORLEVEL 1 goto err
 
 echo Program testing succeeded.
