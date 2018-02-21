d7f7558ed6b4f5a8ad64b0610c8bc0845531daf9
Michael <moskvichevmihail60@yandex.ru>
2018-02-21 12:35:20 +0300

    Добавлены небольшие правки в программу умножения матриц

--- labs/lab1/multmatrix/multmatrix/test.bat
+++ labs/lab1/multmatrix/multmatrix/test.bat
@@ -1,24 +1,29 @@
 set PROGRAM="%~1"
 
-rem ╧ЁютхЁх хь чряєёъ схч ярЁрьхЄЁют ъюьрэфэющ ёЄЁюъш
+rem Проверяем запуск без параметров командной строки
+echo Проверяем запуск без параметров командной строки 
 %PROGRAM% > output.txt
 if not errorlevel 1 goto err
 
-rem ╧ЁютхЁ хь чряєёъ ё эхёє∙хёЄтє■∙шь Їрщыюь
+rem Проверяем запуск с несуществующим файлом
+echo Проверяем запуск с несуществующим файлом
 %PROGRAM% undefined.txt matrix2.txt > output.txt
 if not errorlevel 1 goto err 
 
-rem ╧ЁютхЁ хь чряєёъ ё эхяюыэющ ьрЄЁшЎющ
+rem Проверяем запуск с неполной матрицой
+echo Проверяем запуск с неполной матрицой
 %PROGRAM% incompletematrix.txt > output.txt
 if not errorlevel 1 goto err 
                                             
-rem  ╧ЁютхЁ хь чряєёъ ё ьрЄЁшЎрьш ш ёЁртэштрхь юЄтхЄ
+rem  Проверяем запуск с матрицами и сравниваем ответ
+echo Проверяем запуск с матрицами и сравниваем ответ
 %PROGRAM% matrix1.txt matrix2.txt > output.txt
 if errorlevel 1 goto err 
 fc output.txt multmatrix12.txt
 if errorlevel 1 goto err 
 
-rem ╧ютЄюЁэр  яЁютхЁър ё ьрЄЁшЎрьш ш ёЁртэхэшх юЄтхЄр
+rem Повторная проверка с матрицами и сравнение ответа
+echo Повторная проверка с матрицами и сравнение ответа
 %PROGRAM% matrix3.txt matrix4.txt > output.txt
 if errorlevel 1 goto err 
 fc output.txt multmatrix34.txt
